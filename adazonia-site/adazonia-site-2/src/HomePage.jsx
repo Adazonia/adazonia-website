2979c7c7b51ce95daccf47c0c56659ed30bb54ed
Adazonia <emirhan@adazonia.com>
2025-06-30 19:49:51 +0300

    Add files via upload

---
+++ adazonia-site/adazonia-site-2/src/HomePage.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+// ... Rest of the homepage code (shortened for brevity)
+export default function HomePage() {
+  return (
+    <main>
+      {/* Content */}
+    </main>
+  );
+}
