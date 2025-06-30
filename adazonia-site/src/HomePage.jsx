6440daec11b8a077dbb5e3962dd7b5a12d4b33f1
Adazonia <emirhan@adazonia.com>
2025-06-30 19:55:33 +0300

    Add files via upload

---
+++ adazonia-site/src/HomePage.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function HomePage() {
+  // Canvas code content goes here
+  return (
+    <main>
+      <h1>AdAzonia Homepage</h1>
+    </main>
+  );
+}
